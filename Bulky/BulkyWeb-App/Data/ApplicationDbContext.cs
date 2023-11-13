5f645d6d039a93b287d3493d142690edb576853c
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-11-13 14:46:22 +0200

    building connection string, application db context, add a service to use sql server and connection string

---
+++ Bulky/BulkyWeb-App/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb_App.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
