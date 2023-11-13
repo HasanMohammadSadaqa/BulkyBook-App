960fa77d4010a1591f00738202a89861b96d5af1
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-11-13 14:12:28 +0200

    adding properties to categories model

--- Bulky/BulkyWeb-App/Models/Categories.cs
+++ Bulky/BulkyWeb-App/Models/Categories.cs
@@ -1,6 +1,13 @@
-﻿namespace BulkyWeb_App.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb_App.Models
 {
     public class Categories
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
