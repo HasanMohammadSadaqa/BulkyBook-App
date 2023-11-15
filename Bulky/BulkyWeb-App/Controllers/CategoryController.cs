3e2f27c2e6f2b593dc7cd88fd482a882d28afee8
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-11-15 18:08:10 +0200

    add three records to categories table, and retrieve them by building controller and view for Category

--- Bulky/BulkyWeb-App/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb-App/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb_App.Data;
+using BulkyWeb_App.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb_App.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objListCategories = _context.Categories.ToList();
+            return View(objListCategories);
         }
     }
 }
--- Bulky/BulkyWeb-App/Data/ApplicationDbContext.cs
+++ Bulky/BulkyWeb-App/Data/ApplicationDbContext.cs
@@ -11,5 +11,20 @@ namespace BulkyWeb_App.Data
         }
 
         public DbSet<Category>  Categories { get; set; }
+
+
+        // this is an override for creating model to add records to tables in database 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+                        //this is to add records (new objects) in Categories table
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+
+                );
+        }
     }
 }
