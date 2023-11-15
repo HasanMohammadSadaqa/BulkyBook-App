36684df6fd6e9a20715d3bf62b00b28db7dd297e
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-11-15 20:30:37 +0200

    create a new view to submit a form by creating method in Category Controller

--- Bulky/BulkyWeb-App/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb-App/Controllers/CategoryController.cs
@@ -7,14 +7,40 @@ namespace BulkyWeb_App.Controllers
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _context;
+
+        //dependancy injection (constructor take an object as parameter >>>>> where the dependencies of a class are provided from the outside rather than created within the class itself)
         public CategoryController(ApplicationDbContext context)
         {
             _context = context;
         }
+
+
+        //Action method to retrieve all categories 
         public IActionResult Index()
         {
             List<Category> objListCategories = _context.Categories.ToList();
             return View(objListCategories);
+        }
+
+
+
+        //Action methods to create new Category (2 action method with same name)
+
+        //the first method is to display the view (the form)
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //the second method to create the new category object and save it to database
+
+        [HttpPost]   //When you decorate a controller action method with [HttpPost], you're specifying that this method should only be invoked in response to an HTTP POST request.This means that it will handle form submissions, where data is sent to the server using the POST method, typically to perform create or update operations, among other things.
+
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Category");          // here we redirect to the list of category >>>>> the first qutation is name of view, and the second one is the name of controller
         }
     }
 }
--- Bulky/BulkyWeb-App/Models/Category.cs
+++ Bulky/BulkyWeb-App/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 
 namespace BulkyWeb_App.Models
@@ -9,7 +10,10 @@ namespace BulkyWeb_App.Models
         public int Id { get; set; }
         [Required]
         [NotNull]
+        [DisplayName("Category Name")]    //this is an annotation to display the field as inside the qutation
         public string Name { get; set; }
+
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
