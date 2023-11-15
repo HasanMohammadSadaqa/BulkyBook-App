2a585d65579c0bd2cff60a3721cc6faa39b7f067
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-11-15 11:51:50 +0200

    adding category controller and category views, introducing to CRUD operation on Category table

---
+++ Bulky/BulkyWeb-App/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb_App.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
