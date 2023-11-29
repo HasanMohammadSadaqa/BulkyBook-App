fb6b91b9aeb97b702d521f44723fb5776925fbef
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-11-29 14:03:58 +0200

    add Category Model in razor project

---
+++ Bulky/BulkyWebRazor-Temp/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        [MaxLength(30, ErrorMessage = "Category Name must be less than 30 Characters")]
+        [DisplayName("Category Name")]    //this is an annotation to display the field as inside the qutation
+        public string Name { get; set; }
+
+
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
