97bbf59fe3f6ea0051b87641acce2400536438c7
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-12-03 11:46:39 +0200

    CRUD operation in Razor Project

--- Bulky/BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ Bulky/BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
 
 namespace BulkyWebRazor_Temp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly BulkyWebRazor_Temp.Data.AppDbContext _context;
+
+        public IndexModel(BulkyWebRazor_Temp.Data.AppDbContext context)
         {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Categories != null)
+            {
+                Category = await _context.Categories.ToListAsync();
+            }
         }
     }
 }
