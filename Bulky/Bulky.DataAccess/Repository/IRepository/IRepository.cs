4795d02bb15e825bb69f0fa002c9a5ef21102d35
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-12-05 15:17:05 +0200

    Repository Patern

---
+++ Bulky/Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();                                          //this is a method to retrieve all "T" objects 
+        T GetOneElement(Expression<Func<T, bool>> filter);                // this is a method to retrieve one "T" object >>>>>> there is 3 methods to do that, 1) Find(primary key), 2) FirstOrDefault(link operation), 3) where (link operation).FirstOrDefault >>>>> Here we use First or default and this is the general syntax for that 
+        void AddOneElement(T entity);                                     // this is a method to add an object to database
+        void RemoveOneElement(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ Bulky/Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,53 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;                       //Dependancy Injection
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            this.dbSet = _context.Set<T>();
+            //_context.Categories = dbSet;
+        }
+
+        public void AddOneElement(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public T GetOneElement(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet.Where(filter);
+            //IQueryable<T> query = dbSet;                  >>> I want to pecific this point in 2 steps
+            //query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public void RemoveOneElement(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
