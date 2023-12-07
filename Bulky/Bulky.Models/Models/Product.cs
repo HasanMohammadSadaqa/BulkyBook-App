61fe40cbcd2bda9360340755ee910f42a6ab346a
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2023-12-07 18:52:20 +0200

    CRUD operation for Product

--- Bulky/Bulky.Models/Models/Product.cs
+++ Bulky/Bulky.Models/Models/Product.cs
@@ -1,20 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bulky.Models.Models
 {
-    internal class Product
+    public class Product
     {
         [Key]
         public int Id { get; set; }
 
         [Required]
-        [MaxLength(250)]
+        [MaxLength(250, ErrorMessage = "Product Tilte must be less than 250 Characters")]
         public string Title { get; set; }
 
         [Required]
@@ -29,25 +31,35 @@ namespace Bulky.Models.Models
 
         [Required]
         [DisplayName("List Price")]
-        [Range(1,1000)]
+        [Range(1,1000, ErrorMessage = "List Price must be between 1 and 1000.")]
         public double ListPrice { get; set; }
 
 
         [Required]
         [DisplayName("Price for 1-50")]
-        [Range(1, 1000)]
+        [Range(1, 1000, ErrorMessage = "Price for 1-50 must be between 1 and 1000.")]
         public double Price { get; set; }
 
 
         [Required]
         [DisplayName("Price for 50-100")]
-        [Range(1, 1000)]
+        [Range(1, 1000, ErrorMessage = "Price for 50-100 must be between 1 and 1000.")]
         public double Price50 { get; set; }
 
 
         [Required]
         [DisplayName("Price for 100+")]
-        [Range(1, 1000)]
+        [Range(1, 1000, ErrorMessage = "Price for 100+ must be between 1 and 1000.")]
         public double Price100 { get; set; }
+
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+
+
+        [ValidateNever]
+        public string ImageUrl { get; set; }
     }
 }
--- Bulky/BulkyWeb-App/Areas/Admin/Controllers/ProductController.cs
+++ Bulky/BulkyWeb-App/Areas/Admin/Controllers/ProductController.cs
@@ -1,13 +1,117 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.Models;
+using Bulky.Models.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BulkyWeb_App.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ProductController : Controller
     {
-        [Area("Admin")]
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        } 
+
+
         public IActionResult Index()
         {
+            List<Product> objListOfproducts = _unitOfWork.Product.GetAll().ToList();
+            return View(objListOfproducts);
+        }
+
+
+
+        public IActionResult Create()
+        {
+            ProductVM productVM = new ProductVM()
+            {
+                product = new Product(),
+                categoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+            };
+
+            //ViewBag.CategoryList = CategoryList;
+            return View(productVM);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductVM productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.AddOneElement(productVM.product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index", "Product");
+            }
+            else
+            {
+                productVM.categoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+                return View(productVM);
+            }
+            
+            
+        }
+
+        public IActionResult Edit(int? ProductIdToEdit)
+        {
+            if (ProductIdToEdit == null || ProductIdToEdit == 0)
+            {
+                return NotFound($"Product with Id = {ProductIdToEdit} not found");
+            }
+
+            Product? ProductFromDbToEdit = _unitOfWork.Product.GetOneElement(u=>u.Id == ProductIdToEdit);
+            if(ProductFromDbToEdit == null)
+            {
+                return NotFound($"Product with Id = {ProductIdToEdit} not found");
+            }
+            return View(ProductFromDbToEdit);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product productToEdit)
+        {
+            if(ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(productToEdit);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction("Index", "Product");
+            }
             return View();
+        }
+
+        public IActionResult Delete(int? productIdToDelete)
+        {
+            if (productIdToDelete == null || productIdToDelete == 0)
+            {
+                return NotFound($"Product with Id = {productIdToDelete} not found");
+            }
+
+            Product? ProductFromDbToDelete = _unitOfWork.Product.GetOneElement(u => u.Id == productIdToDelete);
+            if (ProductFromDbToDelete == null)
+            {
+                return NotFound($"Product with Id = {productIdToDelete} not found");
+            }
+            return View(ProductFromDbToDelete);
+        }
+        [HttpPost]
+        public IActionResult Delete(Product productToDelete)
+        {
+                _unitOfWork.Product.RemoveOneElement(productToDelete);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Deleted successfully";
+                return RedirectToAction("Index", "Product");
         }
     }
 }
